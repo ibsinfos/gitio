20cf0bd8b1c511a5bb44f9002f9f2cc0c6d8af3c
Gartndev1\Gartndev-1 <gauravpanwar99@gmail.com>
2017-04-21 05:03:09 +0530

    sdfs

---
+++ instatute-new/insta-tute/terms-and-conditions.php
@@ -0,0 +1,56 @@
+<?php include('header.php'); ?>
+            <div id="faq-tutor-page">
+                <div class="parallax">
+                    <div class="overlay">
+                        <h1 class="page-title-h1">FAQ's:&nbsp;Tutors</h1>
+                    </div>
+                    
+                </div>
+<!--		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+                                    <img src="images/bgimage.jpg" class="background_image">
+				</div>
+			</div>
+		</div>-->
+		
+			<div class="container">
+				<div class="row">
+				<div class="col-md-12">
+					<div class="border_bottom1"></div>
+						<h3 class="heading">How does this work?</h3>
+						<p class="paragraph">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p><br/>
+						<p class="paragraph">It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+					<div class="border_bottom"></div>
+					<h3 class="heading">How do i get paid?</h3>
+					<p class="paragraph">It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+					
+					<div class="border_bottom"></div>
+					<h3 class="heading">How does rating and reviewing work?</h3>
+					<p class="paragraph">It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+					<p class="paragraph">It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+					
+					<div class="border_bottom"></div>
+					<h3 class="heading">What is my status?</h3>
+					<p class="paragraph">It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+					<h4><span style="color:#7E6F42;">Bronze Status:</span>50 - 100 Hours</h4>
+					<h4><span style="color:#D1CDCD;">Silver Status:</span>101 - 250 Hours</h4>
+					<h4><span style="color:#C1AC52;">Gold Status:</span>251 - 1000 Hours</h4>
+					<h4><span style="color:#6F6F6F;">Platinum:</span> 1001 Hours +</h4>
+					
+					<div class="border_bottom"></div>
+					<h3 class="heading">What if my subjects are not listed?</h3>
+					<p class="paragraph">It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+					
+					<div class="border_bottom"></div>
+					<h3 class="heading">Why do i need a Working With a children check?</h3>
+					<p class="paragraph">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+					
+					<div class="border_bottom"></div>
+					<h3 class="heading">Disputes</h3>
+					<p class="paragraph">It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+				</div>
+			</div>
+		</div>
+        </div>
+           <?php include('footer.php'); ?> 
