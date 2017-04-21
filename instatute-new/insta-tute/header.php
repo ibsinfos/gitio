6c5925461be07d5d2caf0cd986645d1b92db26c1
gauravpanwar99 <gauravpanwar99@gmail.com>
2017-04-21 10:10:41 +0530

    sdf

--- instatute-new/insta-tute/header.php
+++ instatute-new/insta-tute/header.php
@@ -1,38 +1,47 @@
 <html>
-	<head>
-		<link href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,500,500i,700,700i" rel="stylesheet">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-		<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
-		<link rel="stylesheet" type="text/css" href="css/style.css">
-		<!--<link rel="stylesheet" type="text/css" href="custom_style.css">-->
-	</head>
-	<body>
-		<nav class="navbar navbar-default navbar-fixed-top">
-                    <div class="row">
-			<div class="col-md-12">
-				<div class="col-md-2">
-                                    <a class="logo" href="#"><img src="images/logo.png"></a>
-				</div>
-				<div class="col-md-8">
-					<ul class="nav navbar-nav">
-						<li class="active"><a href="#">ABOUT US</a></li>
-						<li><a href="#">SPECIAL OFFER</a></li>
-						<li><a href="#">CONTACT US</a></li>
-						<li><a href="#">TUTORS</a></li>
-						<li><a href="#">FOR STUDENTS OR PARENTS</a></li>
-						<li><a href="#">MORE</a></li>
-					</ul>
-				</div>
-                            <div class="col-md-2 social">
-                                    <ul class="social-links">
-						
-                                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-						<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-						<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-					</ul>
-				</div>
-			</div>
+    <head>
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,500,500i,700,700i" rel="stylesheet">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <!--<link rel="stylesheet" type="text/css" href="custom_style.css">-->
+    </head>
+    <body>
+        <nav class="navbar navbar-default navbar-fixed-top">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="col-md-2">
+                        <a class="logo" href="#"><img src="images/logo.png"></a>
                     </div>
-		</nav>
+                    <div class="col-md-8">
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="#">ABOUT US</a></li>
+                            <li><a href="#">SPECIAL OFFER</a></li>
+                            <li><a href="#">CONTACT US</a></li>
+                            <li><a href="#">TUTORS</a></li>
+                            <li class="dropdown">
+                                <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                                    Dropdown
+                                </a>
+                                <ul class="dropdown-menu" role="menu">
+                                    <li><a href="#">Action</a></li>
+                                    <li><a href="#">Another action</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">FOR STUDENTS OR PARENTS</a></li>
+                            <li><a href="#">MORE</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-2 social">
+                        <ul class="social-links">
+
+                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
