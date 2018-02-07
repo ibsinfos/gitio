d71d65060de40eec5f8ead2036805fae358828c0
gauravpanwar99 <gauravpanwar99@gmail.com>
2018-02-07 11:18:38 +0530

    Sd

--- TwistFox/twistfox/wp-content/themes/porto/form-processing.php
+++
@@ -1,4 +0,0 @@
-<?php 
-/* Template Name: Form Processing */
-
-//echo(do_shortcode('display_form_frontend'));
--- twistfox/twistfox/wp-content/themes/porto/page-visatype.php
+++
@@ -1,5 +0,0 @@
-<?php /* Template Name: Visa Type */ ?>
-<?php get_header(); ?>
-<?php the_content(); ?>
-<?php get_footer(); ?>
-
